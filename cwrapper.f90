a881550ded5a0bf100ce1b070220865a55a7d60c
hart <hart@55a9f988-e549-0410-8b28-d6fb27d14cbf>
2012-04-21 00:57:24 +0000

    Still working

--- cwrapper.f90
+++ cwrapper.f90
@@ -20,5 +20,12 @@ if (equal(determinant(A),0._dp,1.e-12_dp)) stop "Matrix is singular in Ccall2_re
 call reduce_to_shortest_basis(A,B,eps)
 END SUBROUTINE aflow_reduce_to_shortest_basis
 
+SUBROUTINE aflow_reduce_to_shortest_basis2(A,B,eps)
+real(dp) :: A(3,3), B(3,3), eps
+print*,"eps",eps
+if (equal(determinant(A),0._dp,1.e-12_dp)) stop "Matrix is singular in Ccall2_reduce"
+call reduce_to_shortest_basis(A,B,eps)
+END SUBROUTINE aflow_reduce_to_shortest_basis2
+
 END MODULE cwrapper
 
