40f3774e3e4487b1f3528b8c83893f7239c8fa5d
hart <hart@55a9f988-e549-0410-8b28-d6fb27d14cbf>
2012-04-21 03:19:59 +0000

    still messing with cwrapper tests

--- trunk/cwrapper.f90
+++ trunk/cwrapper.f90
@@ -22,7 +22,7 @@ call reduce_to_shortest_basis(A,B,eps)
 END SUBROUTINE aflow_reduce_to_shortest_basis
 
 SUBROUTINE aflow_reduce_to_shortest_basis2(rA,cA,A,rB,cB,B,eps)
-real(dp) :: A(rA,cA), eps
+real(dp) :: A(rA,cA), eps, BB(3,3)
 real(dp), pointer :: B(:,:)
 !real(dp), intent(out) ::  B(:,:)
 integer  :: rA, cA, rB, cB, i
@@ -33,7 +33,8 @@ write(*,'("A",/,3(f7.4,1x))') (A(:,i),i=1,3)
 !write(*,'("B",/,3(f7.4,1x))') (B(:,i),i=1,3)
 if (equal(determinant(A),0._dp,1.e-12_dp)) stop "Matrix is singular in aflow_reduce_to_shortest_basi&
      &s2"
-call reduce_to_shortest_basis(A,B,eps)
+call reduce_to_shortest_basis(A,BB,eps)
+B=BB
 rB=size(B,1); cB=size(B,2)
 END SUBROUTINE aflow_reduce_to_shortest_basis2
 
